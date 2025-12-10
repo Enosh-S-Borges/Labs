eac62da7f549719f881dc5e9cef7c483911c5073
Enosh-S-Borges <enosh.s.borges@gmail.com>
2025-12-10 22:49:05 +0530

    Corretced comment Paragraph

--- Lab9.cc
+++ Lab9.cc
@@ -1,5 +1,9 @@
-/*9 Suppose we have three classes Vehicle, FourWheeler, and Car. The class Vehicle is the base class, the class FourWheeler is derived from it and the class Car is derived from the class FourWheeler. Class Vehicle has a method 'vehicle' that prints 'I am a vehicle', class FourWheeler has a method 'fourWheeler' that prints 'I have four wheels', and class Car has a method 'car' that prints 'I am a car'. So, as this is a multi-level inheritance; we can have access to all the other classes methods from the object of the class Car. We invoke all the methods from a Car object and print the corresponding outputs of the methods. So, if we invoke the 
-methods in this order, car(), fourWheeler(), and vehicle(), then the output will be, 
+/*9 Suppose we have three classes Vehicle, FourWheeler, and Car. The class Vehicle is the base class, the class FourWheeler is 
+derived from it and the class Car is derived from the class FourWheeler. Class Vehicle has a method 'vehicle' that prints 'I am a 
+vehicle', class FourWheeler has a method 'fourWheeler' that prints 'I have four wheels', and class Car has a method 'car' that 
+prints 'I am a car'. So, as this is a multi-level inheritance; we can have access to all the other classes methods from the 
+object of the class Car. We invoke all the methods from a Car object and print the corresponding outputs of the methods. 
+So, if we invoke the methods in this order, car(), fourWheeler(), and vehicle(), then the output will be, 
 • I am a car  
 • I have four wheels  
 • I am a vehicle  
