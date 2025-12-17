3e47d8e2e59f21d01a9089f61cb49b869bddfcb4
Enosh S Borges <enosh.s.borges@gmail.com>
2025-12-18 05:23:42 +0530

    Added question comment

--- CDLLH.c
+++ CDLLH.c
@@ -1,3 +1,15 @@
+/* LabProgram-7:
+Develop a menu driven program to implement Circular Double linked list
+with Header node to perform various operations such as
+i) Insertion and Deletion at front/rear
+ii) Insertion and Deletion at the specified position
+iii) Delete by Key
+iv) Search by key
+v) Create an ordered list
+vi) Reverse a list and
+vii) Creating a copy of the list
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
